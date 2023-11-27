a66cd7bffdc8d519d2585a9bcbb368c2bb119c26
El Mahjoub Bouboul <ebouboul@e2r9p1.1337.ma>
2023-11-28 00:13:27 +0100

    ready to be pushed

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ebouboul <ebouboul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 21:56:06 by ebouboul          #+#    #+#             */
-/*   Updated: 2023/11/26 21:56:37 by ebouboul         ###   ########.fr       */
+/*   Updated: 2023/11/28 00:09:30 by ebouboul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,15 @@ int	ft_printf(const char *input_str, ...)
 	{
 		if (input_str[i] == '%')
 		{
+			if (input_str[i + 1] == '\0')
+				return (count);
 			count += check_format(input_str, params, ++i);
-			if (count < 0)
-				return (-1);
 		}
 		else
-		{
 			count += ft_putchar_c(input_str[i]);
-			if (count < 0)
-				return (-1);
-		}
-		i++;
+		if (count < 0)
+			return (-1);
+			i++;
 	}
 	va_end(params);
 	return (count);
@@ -55,7 +53,7 @@ static int	check_format(const char *str, va_list par, int i)
 	else if (str[i] == 'd' || str[i] == 'i')
 		count += ft_putnbr_d(va_arg(par, int));
 	else if (str[i] == 'u')
-		count += ft_putnbr_unsigned((unsigned int)va_arg(par, unsigned int));
+		count += ft_putnbr_unsigned(va_arg(par, unsigned int));
 	else if (str[i] == 'x' || str[i] == 'X')
 		count += ft_puthex_x(va_arg(par, unsigned int), str[i]);
 	else if (str[i] == 'p')
@@ -64,12 +62,3 @@ static int	check_format(const char *str, va_list par, int i)
 		count += ft_putchar_c('%');
 	return (count);
 }
-/*
-#include <stdio.h>
-
-int main ()
-{
-    int a = ft_printf("%s", (char *)NULL);
-
-    printf("\n%d\n", a);
-}*/
